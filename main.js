442d2ec357bfde2d6adff9d08e9c1f8b971af4f9
Chris Postma <cjrpostma@gmail.com>
2019-11-19 19:29:01 -0500

    Add validation module, range and guess type checking

--- main.js
+++ main.js
@@ -1,12 +1,60 @@
 $(document).ready(function () {
 
-  // Module to set state of range, random number
-  const rangeModule = (function () {
+/* =================== module - validates inputs =================== */
+  const validateModule = (function () {
     const DOM = {};
-    let range = {
-      min: 1,
-      max: 100
+    let numberInputs = ['min-range', 'max-range', 'challenger-1-guess', 'challenger-2-guess'];
+    let invalidNumberInputChars = ['e', 'E', '-', ',', '.', '=', '+'];
+
+    /* =================== private methods ================= */
+    function cacheDom() {
+      DOM.$main = $('.main');
+    }
+
+    function bindEvents() {
+      DOM.$main.on('keydown', handleKeydown);
+    }
+
+    function handleKeydown(e) {
+      if (numberInputs.includes(e.target.id) && invalidNumberInputChars.includes(e.key)) {
+        e.preventDefault();
+      }
+    }
+
+    // Min range and max range can't be blank and click submit
+    // Min range, max range, guesses must be number (no symbols)
+    // Min must be less than max
+    // Min, max cannot be negative
+    // Max cannot be greater than 1000
+    // Blank name
+    // Blank guess
+    // Challenger names must be letters (no symbols or numbers)
+    // Challenger names must be <= 15 chars
+
+    // Render errors
+    // Clear errors
+
+    /* =================== public methods ================== */
+    function init() {
+      cacheDom();
+      bindEvents();
+    }
+
+    /* =============== export public methods =============== */
+    return {
+      init: init
     };
+  })();
+
+
+/* =================== module - range, rand num state =================== */
+    const rangeModule = (function () {
+      const DOM = {};
+      let range = {
+        min: 1,
+        max: 100
+      };
+      let randomNumber = setRandomNumber()
 
     /* =================== private methods ================= */
     function cacheDom() {
@@ -24,8 +72,9 @@ $(document).ready(function () {
     function handleClick() {
       setRange();
       resetRangeInputs();
-      setRandomNumber();
+      randomNumber = setRandomNumber();
       renderRangeDisplay();
+      console.log(randomNumber);
     }
 
     function resetRangeInputs() {
@@ -34,11 +83,13 @@ $(document).ready(function () {
     }
 
     /* =================== public methods ================== */
-    function setRange() {
-      range = {
-        min: parseInt(DOM.$userMinRange.val()),
-        max: parseInt(DOM.$userMaxRange.val())
-      };
+    function getRandomNumber() {
+      return randomNumber
+    }
+
+    function renderRangeDisplay() {
+      DOM.$minRangeDisplay.text(range.min);
+      DOM.$maxRangeDisplay.text(range.max);
     }
 
     function resetRange() {
@@ -52,19 +103,24 @@ $(document).ready(function () {
       return Math.floor(Math.random() * (range.max - range.min + 1) + range.min);
     }
 
-    function renderRangeDisplay() {
-      DOM.$minRangeDisplay.text(range.min);
-      DOM.$maxRangeDisplay.text(range.max);
+    function setRange() {
+      range = {
+        min: parseInt(DOM.$userMinRange.val()),
+        max: parseInt(DOM.$userMaxRange.val())
+      };
     }
 
     function init() {
       cacheDom();
       bindEvents();
+      setRandomNumber();
+      renderRangeDisplay();
     }
 
     /* =============== export public methods =============== */
     return {
       init: init,
+      getRandomNumber: getRandomNumber,
       range: range,
       renderRangeDisplay: renderRangeDisplay,
       setRandomNumber: setRandomNumber
@@ -72,31 +128,10 @@ $(document).ready(function () {
   })();
 
 
-
-
-
-  /* =============== export public methods =============== */
-  rangeModule.init();
+/* =============== program execution =============== */
+  validateModule.init();
   // Program starts with default range 1-100
   // Program starts with random number between default 1-100
-  rangeModule.setRandomNumber();
-  rangeModule.renderRangeDisplay();
-
-
-
-
-  const $main = $('.main');
-
-  let numberInputs = ['min-range', 'max-range', 'challenger-1-guess', 'challenger-2-guess'];
-  let invalidNumberInputChars = ['e', 'E', '-', ',', '.', '=', '+'];
-
-  // As a user, the game selects a random number between the minRange and maxRange
-
-
-  $main.on('keydown', (e) => {
-    if (numberInputs.includes(e.target.id) && invalidNumberInputChars.includes(e.key)) {
-      e.preventDefault();
-    }
-  });
+  rangeModule.init();
 
 });
